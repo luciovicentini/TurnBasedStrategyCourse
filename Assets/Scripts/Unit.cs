c77c97d0506b02e647c64d52664397e1524b36d3
Lucio <luchovicentini@gmail.com>
2022-07-13 13:26:42 +0200

    22_Unit Rotate when Moving

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -11,20 +11,33 @@ public class Unit : MonoBehaviour {
         if (Input.GetMouseButtonDown(0)) {
             targetPosition = MouseWorld.GetPosition();
         }
-        
+
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) < stoppingDistance) {
             unitAnimator.SetBool("isWalking", false);
-        } else {
-            Move();
+        }
+        else {
+            MoveAndRotate();
         }
     }
 
-    private void Move() {
+    private void MoveAndRotate() {
         Vector3 moveDir = (targetPosition - transform.position).normalized;
-        float moveSpeed = 4f;
-        transform.position += moveDir * (Time.deltaTime * moveSpeed);
-        
+        Move(moveDir);
+        Rotate(moveDir);
         unitAnimator.SetBool("isWalking", true);
     }
+
+    private void Move(Vector3 moveDir) {
+        float moveSpeed = 4f;
+        transform.position += moveDir * (Time.deltaTime * moveSpeed);
+    }
+
+    private void Rotate(Vector3 direction) {
+        float rotateSpeed = 3f;
+        Debug.Log($"t = {Time.deltaTime * rotateSpeed}");
+        Debug.Log($"transform.forward = {transform.forward}");
+        transform.forward = Vector3.Lerp(transform.forward, direction, (Time.deltaTime * rotateSpeed));
+    }
+
 }
