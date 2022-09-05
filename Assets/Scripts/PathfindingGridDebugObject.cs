5d879cf40f1d4c960e0128e7fbde68c64653fbdc
Lucio <luchovicentini@gmail.com>
2022-09-05 14:53:04 +0200

    67_Level Design

--- Assets/Scripts/PathfindingGridDebugObject.cs
+++ Assets/Scripts/PathfindingGridDebugObject.cs
@@ -7,7 +7,7 @@ public class PathfindingGridDebugObject : GridDebugObject {
     [SerializeField] private TextMeshPro gCostText;
     [SerializeField] private TextMeshPro hCostText;
     [SerializeField] private TextMeshPro fCostText;
-
+    [SerializeField] private SpriteRenderer isWalkableSpriteRenderer;
     private PathNode pathNode;
 
     public override void SetGridObject(object gridObject) {
@@ -20,5 +20,7 @@ public class PathfindingGridDebugObject : GridDebugObject {
         gCostText.text = pathNode.GetGCost().ToString();
         hCostText.text = pathNode.GetHCost().ToString();
         fCostText.text = pathNode.GetFCost().ToString();
+
+        isWalkableSpriteRenderer.color = pathNode.IsWalkable() ? Color.green : Color.red;
     }
 }
