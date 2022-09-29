37d1d64dd1ba798be7b731e6fc7fa08800c432d5
Lucio <luchovicentini@gmail.com>
2022-09-29 14:33:37 +0200

    74 - Destructible Crate Parts

--- Assets/Scripts/DestructibleCrate.cs
+++ Assets/Scripts/DestructibleCrate.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class DestructibleCrate : MonoBehaviour {
     public static event EventHandler OnAnyDestroyed;
+
+    [SerializeField] private Transform crateDestroyedPrefab;
     
     private GridPosition _gridPosition;
 
@@ -13,8 +15,37 @@ public class DestructibleCrate : MonoBehaviour {
     }
 
     public void Damage() {
-        Destroy(gameObject);
+        Transform crateDestroyedTransform = Instantiate(crateDestroyedPrefab, transform.position, Quaternion.identity);
+        ApplyExplosionToChildren(crateDestroyedTransform, 150f, transform.position, 10f);
+
         OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+        transform.GetChild(0).gameObject.SetActive(false);
+        StartCoroutine(RemoveDestroyedCrate(crateDestroyedTransform));
+    }
+
+    private IEnumerator RemoveDestroyedCrate(Transform crateDestroyedTransform) {
+        float elapsedTime = 0f;
+        float removeCrateTimer = 2f;
+        
+        while (elapsedTime <= removeCrateTimer) {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        // yield return new WaitForSeconds(removeCrateTimer);
+        
+        Destroy(crateDestroyedTransform.gameObject);
+        Destroy(gameObject);
+    }
+
+    private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition,
+        float explosionRadius) {
+        foreach (Transform child in root) {
+            if (child.TryGetComponent(out Rigidbody childRigidBody)) {
+                childRigidBody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
+                
+                ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRadius);
+            }
+        }
     }
 
     public GridPosition GetGridPosition() => _gridPosition;
